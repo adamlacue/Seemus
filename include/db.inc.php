bf8f22eab9624d7629df889ac879908a5e8d134d
Adam Lacue <adam.lacue@gmail.com>
2024-01-29 12:06:44 -0500

    move it

--- include/db.inc.php
+++ include/db.inc.php
@@ -2,8 +2,8 @@
 <?php
 
   $servername = "localhost";
-  $username = "selfridge";
-  $password = "xpsr450D$";
+  $username = "shuser";
+  $password = "shRocks!";
   $dbname = "Seemus";
   
 try {
@@ -12,30 +12,6 @@ try {
     echo "Successful connection";
 } catch(PDOException $e) {
     echo $e->getMessage();
-}
-
-$stmt = $conn->prepare("SELECT `IDauto`,`Name` FROM Seemus.tbTable");
-$stmt->execute();
-$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-
-if ($result) {
-  $printedHeader=false;
-  while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-    if(!$printedHeader) {
-      echo "<table>";   // Start Table
-      echo "<tr>";      // Start Header Row
-        foreach($row as $col_name => $val)
-          {
-          echo "<th>$col_name</th>";    // Print Each Field Name
-          }
-      echo "</tr>";
-      $printedHeader=true;
-    }
-    echo "<tr><td>".$row["IDauto"]."</td><td>".$row["Name"]."</td></tr>";
-  }
-  echo "</table>";
-} else {
-  echo "0 results";
 }
 
 ?>
