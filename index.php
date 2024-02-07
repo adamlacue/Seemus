3eaecda7327be0ab9622716dfc5afeb3103ab707
adamlacue <117120196+adamlacue@users.noreply.github.com>
2024-02-07 11:49:43 -0500

    logon button

--- index.php
+++ index.php
@@ -1,6 +1,84 @@
 <?php
 include "include/db.inc.php";
+include "class/Utils.class.php";
+
+// Utils::prettyPrint("SELECT * FROM Seemus.tbTable;")
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
+if(isset($REQUEST["activity"])) {
+    $activity = $_REQUEST["activity"]
+}
+
+?>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <a href="index.php?activity=USER">LOGON</a>
+
+        <?php
+        switch($activity) {
+            case "USER":
+                // User Login
+                if(!isset($_REQUEST["username"])) {
+                    ?>
+                    <form action="index.php?activity=USER" method=get>
+                        <input type="text" name="username" placeholder="Username / Email" />
+                        <input type="submit" value="Logon" />>
+                    </form>
+                    <?php
+                } else {
+                    echo $_REQUEST["username"];
+                }
+            break;
+
+            case "LOGOUT":
+                // User Logout
+
+            break;
+
+            case "VIEW":
+                // View List of Content
+
+            break;
+            
+
+            case "EDIT":
+                // User Edit
+                
+            break;
+
+            case "DELETE":
+                // User Delete
+                
+            break;
+            
+            case "CREATE":
+                // Create Content
+                
+            break;
+            
+
+
+            default:
+                //default viewing of content
+                ?>
+
+
+
+                <?php
+            break;
+        }
+
+        ?>
+    </body>
+</html>
+
+<?php
 include "include/dbclose.inc.php";
 ?>
